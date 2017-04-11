01af2c8d61cbbe3be0aaf9fdd5d58971ac1ebec7
AdeelK <Adeelpkpk@gmail.com>
2017-04-12 02:15:21 +0500

    Added subdomains

--- yapjoy/settings.py
+++ yapjoy/settings.py
@@ -41,7 +41,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['www.yapjoy.com','yapjoy.herokuapp.com','https://s3-us-west-2.amazonaws.com','.facebook.com','http://yui.yahooapis.com','bayareaweddingfairs.herokuapp.com']
+ALLOWED_HOSTS = ['crm.bayareaweddingfairs.com','https://s3-us-west-2.amazonaws.com','.facebook.com','http://yui.yahooapis.com','bayareaweddingfairs.herokuapp.com']
 SITE_NAME = 'https://www.yapjoy.com/'
 
 # Local time zone for this installation. Choices can be found here:
