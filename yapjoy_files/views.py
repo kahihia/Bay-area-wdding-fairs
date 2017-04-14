e38a59042aba792c5ffd3d58ba221b312914b08c
AdeelK <Adeelpkpk@gmail.com>
2017-04-15 00:40:35 +0500

    Clean home view and template

--- yapjoy_files/views.py
+++ yapjoy_files/views.py
@@ -5192,8 +5192,6 @@ def percent_cb(complete, total):
 
 @csrf_exempt
 def bg_managementv2(request):
-    # user = request.user
-    # profile = user.userprofile
     error_message = None
     eventsIDList = None
     cat = ""
@@ -5201,8 +5199,6 @@ def bg_managementv2(request):
     how_heard = None
     inteventsIDList = None
     form = bg_registration_form()
-
-
     if request.method == "POST":
         form = bg_registration_form(request.POST)
         event_name = ""
@@ -5216,13 +5212,8 @@ def bg_managementv2(request):
         else:
             eventsCheck = "Select an event atleast"
         if form.is_valid():
-            print 'form is valid'
             data = form.cleaned_data
-
-            print data
-            print "eventsIDList: ",eventsIDList
             if eventsIDList:
-
                 firstName = data['firstName']
                 lastName = data['lastName']
                 phone = data['phone']
@@ -5233,10 +5224,8 @@ def bg_managementv2(request):
                 how_heard = data['how_heard']
                 categories = data['categories']
                 name = firstName + " " + lastName
-
                 try: weddingDate = data['weddingDate']
                 except: weddingDate = ''
-
                 user_reg = None
                 reg = None
                 try:
@@ -5260,26 +5249,17 @@ def bg_managementv2(request):
                                                         zip=zip,
                                                         comments=comments,
                                                         how_heard=how_heard,
-                                                        # categories=cat,
                                                         event=event,
                                                         weddingDate=weddingDate,
                                                         type=Register_Event.BGUSER,
                                                         )
                     event_name += "%s - %s<br />" % (event.name, event.date)
-
                     for cat in categories:
-                        print cat
-                        # print cat.id
                         try:
                             ca = CategoryOptions.objects.get(category__iexact=cat)
                             reg.categories.add(ca)
                         except:
                             pass
-
-
-
-
-
                 message = "Thank you for registering with Bay Area Wedding Fairs.<br /><br />%s<br /><br />If you like to save time and money, pre-pay online at <a href='http://bayareaweddingfairs.com/bridegroomtestregistration/'>https://bayareaweddingfairs.com/bridegroomtestregistration/</a>. Also, we encourage you to go to our <a href='https://www.facebook.com/baweddingfairs/events'>Facebook Events Page</a>, select the Fair and click on 'Join'. You will be entered in our weekly 'Las Vegas Giveaway' drawing (Winner will be announced on our Facebook page). <br /><br />NEED HELP IN PLANNING? Try our new Wedding Planning Platform <a href='https://www.yapjoy.com/'>YAPJOY</a>.<br /><br />if you have any questions, please email us at <a href='mailto:info@bayareaweddingfairs.com'>info@bayareaweddingfairs.com </a><br /><br />We look forward to seeing you at the Wedding Fair<br /><br />Thank You!<br /><br />Bay Area Wedding Fairs" % (
                 event_name)
                 send_bawf_email(sendTo=user_reg.email, message=message, title="",
@@ -5287,16 +5267,14 @@ def bg_managementv2(request):
                 message_information = "First Name: %s<br />Last Name: %s<br />Email: %s<br />Phone: %s<br />City: %s<br />Zip: %s<br />Wedding Fair Interested In: <br />%s<br />Wedding Professional Interested in: %s<br />How do you hear about us: %s<br />Comments: %s<br />"%(firstName, lastName, email, phone, city, zip, event_name, cat, how_heard, comments)
                 send_bawf_email(sendTo='info@bayareaweddingfairs.com', message=message_information, title="", subject="Bride / Groom Registration Information!")
                 return HttpResponseRedirect('/crm/registration/bg/success/iframe/')
-                # return redirect('http://bayareaweddingfairs.com/bride-groom-ticket-registration/')
             else:
-
                 error_message = "Select an event atleast"
                 eventsCheck = "Select an event atleast"
         else:
             error_message = "Form is not valid: ",form.errors
-
-
-    #coverging events by season
+    """
+    converging events by season
+    """
     events = Event_fairs.objects.filter(Q(is_expired=False)).order_by('date')
     content = {
         'form':form,
@@ -5307,10 +5285,7 @@ def bg_managementv2(request):
         'eventsCheck':eventsCheck,
         'all_events':events,
         'how_heard':how_heard,
-
     }
-
-    template_name = 'vendroid/CRM/bgReg.html'
     return render(request, 'vendroid/CRM/bgRegV2.html', content)
 
 
