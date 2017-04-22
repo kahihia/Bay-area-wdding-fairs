ebbebd56d2dec1a31b7f4d6b5ed3c10506745356
Mobeen <mobeen.ahmed11@yahoo.com>
2017-04-22 21:39:59 +0500

    vendorsShop

--- bayareaweddingfairs_site/urls.py
+++ bayareaweddingfairs_site/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from .views import *
+
 urlpatterns = patterns('',
     url(r'^shop_vendors/', ShopVendors),
     url(r'^$', Index),
--- bayareaweddingfairs_site/views.py
+++ bayareaweddingfairs_site/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
+from .models import *
+
 
 def Index(request):
     return render(request, "bayareaweddingfairs/site/index/index.html")
 
+
 def ShopVendors(request):
-    return render(request, "bayareaweddingfairs/shopVendors/index.html")
+    vendorShopsList = []
+    vendorShops = ShopVendor.objects.all()
+    for shop in vendorShops:
+        shopItemList = ShopVendorsItem.objects.filter(shopVendors=shop).order_by('created_at')
+        vendorShop = {
+            'shop': shop,
+            'itemsList': shopItemList
+        }
+        vendorShopsList.append(vendorShop)
+
+    context = {
+        'vendorShops': vendorShops,
+        'vendorShopsList': vendorShopsList
+    }
+    return render(request, "bayareaweddingfairs/shopVendors/index.html", context)
+    # return render(request, "bayareaweddingfairs/site/index/shopVendors.html", context)
