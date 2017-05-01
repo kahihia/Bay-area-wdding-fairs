59c030fd48c0cd0d2dee669e6b5a7344f1a1d393
AdeelK <Adeelpkpk@gmail.com>
2017-05-01 11:48:52 +0500

    Added privacy policy

--- bayareaweddingfairs_site/urls.py
+++ bayareaweddingfairs_site/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 from .views import *
 
 urlpatterns = patterns('',
@@ -11,6 +12,7 @@ urlpatterns = patterns('',
                        url(r'^bride-groom-ticket/', BrideGroomTicket, name="index__bride_groom_ticket"),
                        # url(r'^bride-groom-ticket-success/', BrideGroomTicketSuccess, name="index__bride_groom_ticket_success"),
                        # url(r'^shop-vendors-details/(?P<id>\d+)/$', shopDetail, name="storefrontItemDetail"),
+                       url(r'^privacy-policy/', TemplateView.as_view(template_name="bayareaweddingfairs/site/PrivacyPolicy/Policy.html"), name="index__PrivacyPolicy"),
                        url(r'^become-an-exhibitor/', BecomeExhibitor, name="index__BecomeExhibitor"),
                        url(r'^registration-success/', VendorRegistrationThankYou, name="index__vendor_registration__success"),
                        url(r'^bride-registration-success/', BrideRegistrationThankYou, name="index__bride_registration__success"),
