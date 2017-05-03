9fcff752e79d3d49cbd3441ff8834c5aaef1545d
AdeelK <Adeelpkpk@gmail.com>
2017-05-03 23:05:17 +0500

    Added stripe test and publishable keys

--- bayareaweddingfairs_site/views.py
+++ bayareaweddingfairs_site/views.py
@@ -247,7 +247,7 @@ def BrideGroomTicket(request):
     Needs to be converted to Live keys via
     settings.STRIPE_SECRET_KEY_BAWF
     """
-    stripe.api_key = 'sk_test_z3b8Yfc0Mcuh0P3M7VDfGZkt'
+    stripe.api_key = settings.STRIPE_SECRET_KEY
     ticketform = CreditCardBAWFTicketForm()
     valid_code = ""
     error_message = ""
@@ -373,7 +373,7 @@ def BrideGroomTicket(request):
         settings.STRIPE_PUBLISHABLE_KEY_BAWF
     """
     context = {
-        'pub_key': 'pk_test_ic11SWVPcUHwZ1mDBEBTdSX1',
+        'pub_key': settings.STRIPE_PUBLISHABLE_KEY,
         'event': Event_fairs.objects.filter(date__gte=datetime.now().date()).filter(amount__isnull=False).filter(Q(standard_ticket_visible=True)|Q(earlybird_ticket_visible=True)|Q(group_ticket_visible=True)).exclude(id__in=[46,47,48,49,50,51,52]).order_by('date'),
         'form': ticketform,
         'hide_thanks': hide_thanks,
