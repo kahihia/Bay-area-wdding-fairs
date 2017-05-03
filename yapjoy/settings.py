315f72639b006b0036e6ec0a31a6e6330a992339
AdeelK <Adeelpkpk@gmail.com>
2017-05-03 23:01:24 +0500

    Updated the settings

--- yapjoy/settings.py
+++ yapjoy/settings.py
@@ -41,8 +41,8 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['crm.bayareaweddingfairs.com','https://s3-us-west-2.amazonaws.com','.facebook.com','http://yui.yahooapis.com','bayareaweddingfairs.herokuapp.com']
-SITE_NAME = 'https://www.yapjoy.com/'
+ALLOWED_HOSTS = ['*.bayareaweddingfairs.com','crm.bayareaweddingfairs.com','https://s3-us-west-2.amazonaws.com','.facebook.com','http://yui.yahooapis.com','bayareaweddingfairs.herokuapp.com']
+SITE_NAME = 'https://www.bayareaweddingfairs.com/'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
