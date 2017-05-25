132fa9ea74d2d8353ff72ae165695e0d6d32e689
MOBEEN AHMED <myousaf@bayareaweddingfairs.com>
2017-05-25 12:50:54 +0500

    permission applied for sub urls

--- yapjoy_files/decorator.py
+++ yapjoy_files/decorator.py
@@ -2,17 +2,22 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.auth.models import User
 from django.template import *
 from yapjoy_files.models import *
+import re
 
 
 def is_permitted(function):
     """decorator for sidebar permission
     
     :param function: 
-    :return: 
+    :return: Permission error/ function
     """
     def wrap(request, *args, **kwargs):
         user = User.objects.get(pk=request.user.id)
-        permission = UserPermissionPage.objects.filter(user=user, crmUrl__url_path__icontains=request.get_full_path, user_allowed=True)
+        print "req: ", request.path
+        urlpath = request.path
+        if hasNumbers(urlpath):
+            urlpath = splitInt(urlpath)
+        permission = UserPermissionPage.objects.filter(user=user, crmUrl__url_path__icontains=urlpath, user_allowed=True)
 
         if user.is_superuser:
             return function(request, *args, **kwargs)
@@ -24,3 +29,23 @@ def is_permitted(function):
     wrap.__doc__ = function.__doc__
     wrap.__name__ = function.__name__
     return wrap
+
+
+def hasNumbers(inputString):
+    """Check the URL int at the end 
+    
+    :param inputString: 
+    :return: True/False
+    """
+    return bool(re.search(r'\d', inputString))
+
+
+def splitInt(inputstring):
+    """ split the integer from url
+    
+    :param inputstring: 
+    :return: string url 
+    """
+    n = re.findall('\d+', inputstring)
+    return inputstring.split(n[0])[0]
+
--- yapjoy_files/templatetags/filterPermissions.py
+++ yapjoy_files/templatetags/filterPermissions.py
@@ -9,17 +9,13 @@ register = template.Library()
 
 @register.filter
 def get_user_permissions(req, args):
-    print "req: ", req, args
     user = User.objects.get(pk=req.user.id)
     permission = UserPermissionPage.objects.filter(user=user, crmUrl__url_path__icontains=args,
                                                    user_allowed=True)
     if user.is_superuser:
-        print "user"
         return True
     elif permission:
-        print "filter"
         return True
 
     else:
-        print "else"
         return False
